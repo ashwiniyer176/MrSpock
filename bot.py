aa46fb1adfaf377ba4ba2ff26c3da2e2d6e4f2e6
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2022-11-06 22:51:49 +0530

    Basic bot setup

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+client.run(os.environ["SECURITY_TOKEN"])
